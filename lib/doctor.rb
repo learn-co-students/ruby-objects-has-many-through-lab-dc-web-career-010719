5facbaeab9e91fadbf99d82156f8c04e33797cc2
gillsinaglass <rdgill95@gmail.com>
2019-01-12 15:24:43 -0500

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,29 @@
+class Doctor
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(name, date)
+      Appointment.new(date,name, self)
+  end
+
+  def appointments
+    return Appointment.all
+  end
+
+  def patients
+    Appointment.all.collect{|apt| apt.patient}
+
+  end
+
+end
